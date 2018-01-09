2492ac904a8bd710166298e1847583a9678933c6
Skurat <skuratovskiy.geek@gmail.com>
2018-01-10 00:00:58 +0200

    Added the script that auto-fill scripts and styles in index.html, added SCSS, included bootstrap, jquery

---
+++ script.js
@@ -0,0 +1,23 @@
+var request = new XMLHttpRequest();
+request.open('GET', 'include.json');
+request.send();
+
+request.onreadystatechange = function(){
+    if(request.readyState != 4){return;}
+    if(request.status != 200){alert(request.status)}
+    else{
+        var incl = JSON.parse(request.responseText);
+        for(let i = 0; i < incl.css.length; i++){
+            var link = document.createElement('link');
+            link.setAttribute('rel', 'stylesheet');
+            link.setAttribute('href', incl.css[i]);
+            console.log(link);
+            document.head.append(link);
+        }
+        for(let i = 0; i < incl.js.length; i++){
+            var script = document.createElement('script');
+            script.setAttribute('src', incl.js[i]);
+            var sd = document.getElementById('scripts');
+            document.body.appendChild(script);
+    }
+    }};
